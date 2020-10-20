9849b4b20a19b3ee08b6a0cdad9afabd442c5652
Dirk Mueller <dirkmuller8@outlook.de>
2020-10-20 13:24:07 +0200

    Added text to markdown

---
+++ LinkedList/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class listElement
+    {
+        // Software for a linked list implementation, where the last element
+        // is appended to the end of the list (tail), without having to rund 
+        // through it from the head.
+        // This was a task as part of the C# developer line at Fernakademie Klett
+        // Dirk Mueller,
+        // Oct 2019
+        //
+        string content;
+        listElement next;
+        public void SetData(string newContent)
+        {
+            content = newContent;
+            next = null;
+        }
+
+        // The Append method requires a return typ of listElement, so that it can 
+        // return the most recently defined list element to Main():
+        public listElement AppendElement(string newContent)
+        {
+            next = new listElement();
+            next.SetData(newContent);
+
+            // The new list element was created in next. Thereby next is the end 
+            // of the list, and can be returned:
+            return next;
+        }
+
+        public void PrintToScreen()
+        {
+            Console.WriteLine(content);
+            if (next != null)
+                next.PrintToScreen();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            listElement headOfList = new listElement();
+            // Instantiate the tail of the list:
+            listElement tailOfList = new listElement();
+
+            headOfList.SetData("Element 1");
+
+            // Set the new instance to the head of the list, so that the tail 
+            // can be defined:
+
+            tailOfList = headOfList;
+
+            // Append element to the end of the list. With each append the 
+            // instance tailOfList is set to the newly formed list element, 
+            // by means of returning the value:
+
+            for (int element = 2; element < 5; element++)
+                tailOfList = tailOfList.AppendElement("Element " + element);
+
+            headOfList.PrintToScreen();
+
+            string inputString;
+            do
+            {
+                Console.WriteLine("Give a new element number (99 to abort): ");
+                inputString = Console.ReadLine();
+
+                // Append the new list element and determine the end of the list:
+                tailOfList = tailOfList.AppendElement("Element " + inputString);
+
+                Console.WriteLine("Tail of list after appending:");
+                tailOfList.PrintToScreen();
+
+                // Append the new list element:
+                Console.WriteLine("Print from head of list after appending:");
+                headOfList.PrintToScreen();
+            }
+            while (inputString != "99");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,30 @@
+## Linked list with direct append to the tail
+**********************************************
+Software:		C# 
+
+Version:    	1.0
+
+Date: 			Oct 13, 2020
+
+Author:			Dirk Mueller
+**********************************************
+Software for a linked list implementation, where the last element is appended to the end of the list (tail), without having to rund 
+through it from the head.
+
+This was a task as part of the C# developer line at Fernakademie Klett.
+
+![](https://github.com/DirkMueller8/linked_list/blob/master/screenshot_code.png)
+
+*Fig 1: Excerpt of code showing the method to append an element to the tail directly*
+
+Example results after running the software:
+
+![](https://github.com/DirkMueller8/python_bikeshare_project/blob/master/snapshot_1.png)
+
+*Fig 2: Example of the software output*
+
+The following course wasused to solve the project:
+
+**Website:**
+
+1. Fernakademie Klett, https://www.fernakademie-klett.de/technik-it/informatik-programmierung-internet/c-software-entwickler/ 
